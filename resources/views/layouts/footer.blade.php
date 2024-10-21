279ce8224ee91849a2239e9fbcacc999a30497df
ashiq33 <202105040085@student-city.edu.my>
2024-10-21 23:00:28 +0800

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+
+     ©  {{date('Y')}} Attendance Management System <span class="d-none d-sm-inline-block"> - By <i class="mdi mdi-heart text-danger"></i> MD ASHIQ SHAHARIAR</span>.
+</footer>
